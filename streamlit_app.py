a5db2d27ecf766a9d8558fb75ee909b99e746efa
inuzuki123 <ilsesprangers1993@gmail.com>
2026-05-14 20:45:29 +0000

    modified:   streamlit_app.py 	modified:   reversed_rf_model.pkl 	modified:   rf_model.pkl 	modified:   streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -63,9 +63,9 @@ if st.button("🚀 Predict Trade"):
     st.write("---")
 
     # beslissing
-    if prediction > reversedPrediction and prediction > 0.171:
+    if prediction > reversedPrediction and prediction > 0.134:
         st.success("✅ TAKE NORMAL")
-    elif reversedPrediction > prediction and reversedPrediction > 0.1666:
+    elif reversedPrediction > prediction and reversedPrediction > 0.1737:
         st.success("🔻 TAKE REVERSED")
     else:
         st.error("❌ SKIP TRADE")
